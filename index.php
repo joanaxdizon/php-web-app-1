985aea4ecc769dd636abc3faf0b72c8a0ddbca63
joanaxdizon <jerlinjoana.dizon@seattlecolleges.edu>
2021-06-03 16:47:22 +0000

    Adds index.php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_GET['FirstName'])){//data exists, show it
+  //echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+
+  echo '<pre>';
+  var_dump($_GET);
+  echo '<pre>';
+
+}else{//no data, show form
+  echo '
+  <form action="">
+  First Name: <input type="text" name="FirstName" /><br />
+  Last Name: <input type="text" name="LastName" /><br />
+  <input type="submit" />
+  </form>
+  ';
+}
+?>
